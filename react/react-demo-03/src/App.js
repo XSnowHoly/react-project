d29ae2f36a78d33dac9f8dd6007284a50e811be1
xsnowholy <495904127@qq.com>
2017-08-18 17:14:08 +0800

    新建leanCloud.js

--- react/react-demo-03/src/App.js
+++ react/react-demo-03/src/App.js
@@ -6,15 +6,6 @@ import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
 import * as localStore from './localStore';
 
-import AV from 'leancloud-storage';
-var APP_ID = 'oCYBSIymXaG9CLFlxu8W6EiG-gzGzoHsz';
-var APP_KEY = 'uxX9vmosB3hslT8NuHC0PPes';
-
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
-
 class App extends Component {
   constructor(props) {
     super(props);
---
+++ react/react-demo-03/src/leanCloud.js
@@ -0,0 +1,8 @@
+import AV from 'leancloud-storage';
+var APP_ID = 'oCYBSIymXaG9CLFlxu8W6EiG-gzGzoHsz';
+var APP_KEY = 'uxX9vmosB3hslT8NuHC0PPes';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
